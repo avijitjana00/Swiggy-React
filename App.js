537c5b0e75a7fb019b06c7989d1b06335c6e9faf
avijit <avijitjana@gmail.com>
2026-01-31 21:15:41 +0530

    installed react, react-dom, parcel

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const HeadingReact = () => <h1>Swiggy React App</h1>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingReact/>);
